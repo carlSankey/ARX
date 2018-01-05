923679dca6e021657d566292db13962065fbcbaf
bob0bob <kevin@freemailclient.com>
2018-01-04 21:53:02 -0600

    project file update

--- src/ARx_private.h
+++ src/ARx_private.h
@@ -5,19 +5,19 @@
 #define ARX_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.82.0.0"
+#define VER_STRING	"0.83.0.0"
 #define VER_MAJOR	0
-#define VER_MINOR	82
+#define VER_MINOR	83
 #define VER_RELEASE	0
 #define VER_BUILD	0
 #define COMPANY_NAME	""
-#define FILE_VERSION	"0.82.0.0"
+#define FILE_VERSION	"0.83.0.0"
 #define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	""
-#define PRODUCT_VERSION	"0.82.0.0"
+#define PRODUCT_VERSION	"0.83.0.0"
 
 #endif /*ARX_PRIVATE_H*/
